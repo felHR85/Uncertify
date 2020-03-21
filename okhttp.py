13de31873eab5a89070db1fb3e9c5079999b9fdc
felHR85 <felhr85@gmail.com>
2020-03-21 21:35:45 +0100

    okhttp modify function working

--- okhttp.py
+++ okhttp.py
@@ -9,9 +9,9 @@ begin_method = ".method public certificatePinner(Lokhttp3/CertificatePinner;)Lok
 end_method = ".end method"
 
 # injects
-line_1 = ".locals 2"
-line_2 = ".param p1, \"certificatePinner\""
-line_3 = "return-object p0"
+line_1 = "\t.locals 2\n"
+line_2 = "\t.param p1, \"certificatePinner\"\n"
+line_3 = "\treturn-object p0\n"
 
 def modify_okhttp(app_folder):
     f = _get_okhttp_file(app_folder)
@@ -25,29 +25,29 @@ def _get_okhttp_file(app_folder):
     for root, dirs, files in os.walk(app_folder):
         if okhttp_file_name in files:
             return os.path.join(root, okhttp_file_name)
-        else:
-            return ''
+    return ''
 
 def _edit_okhttp_file(okhttp_file):
-    i = 1
+    i = 0
     line_begin = 0
     line_end = 0
 
     file_lines = open(okhttp_file, 'r').readlines()
     for line in file_lines:
-        if line == begin_method:
+        if begin_method in line:
             line_begin = i
-        elif line == (end_method and line_begin is not 0):
+        elif (end_method in line) and line_begin is not 0:
             line_end = i
+            break
         i = i +1
-
+    
     file_lines[line_begin + 1] = line_1
     file_lines[line_begin + 2] = line_2
     file_lines[line_begin + 3] = line_3
-
+    
     for j in range(line_begin + 4, line_end):
         file_lines[j] = ""
 
     with open(okhttp_file, 'w') as okhttp_file:
         for item in file_lines:
-            okhttp_file.write(item)
+            okhttp_file.write(item)    
